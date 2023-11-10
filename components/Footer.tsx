b67100d15e6b899c3938736ff668f801fc1775a8
Matej Tancek <mtancek@users.noreply.github.com>
2023-11-10 22:20:16 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,20 +20,16 @@ const Footer: FC<Props> = (props) => (
         {...props}
     >
         <Link href="https://github.com/thedaviddelta/lingva-translate/blob/main/LICENSE" isExternal={true}>
-            <Text as="span">&#169; 2021 thedaviddelta & contributors</Text>
+            <Text as="span">&#169; 2023 thedaviddelta & contributors</Text>
         </Link>
         <Text as="span" display={["none", null, "unset"]}>·</Text>
         <Link href="https://www.gnu.org/licenses/agpl-3.0.html" isExternal={true}>
             <Text as="span">Licensed under AGPLv3</Text>
         </Link>
-        {vercelSponsor && (
-            <>
                 <Text as="span" display={["none", null, "unset"]}>·</Text>
-                <Link href="https://vercel.com?utm_source=lingva-team&utm_campaign=oss" isExternal={true}>
-                    <Text as="span">▲ Powered by Vercel</Text>
+                <Link href="https://www.rsmt.si?utm_source=lingva-translate" isExternal={true}>
+                    <Text as="span">▲ Powered by RSMT</Text>
                 </Link>
-            </>
-        )}
     </Stack>
 );
 
